f2e4c1b4f23e77f87ab25eaae06cdc9c0b3c64fc
Jose <38721575+sharco-code@users.noreply.github.com>
2019-09-26 08:09:04 +0200

    0.3

--- Server.h
+++ Server.h
@@ -1,76 +1,86 @@
 #ifndef SERVER_H
 #define SERVER_H
 
-#include "Socket.h"
+#undef UNICODE
 
+#define _WIN32_WINNT 0x501
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <iostream>
-#include <process.h>
-#include <string>
 
-//static unsigned char DEFAULT_PORT = 80;
-//int portProxy;
-//std::string addrServer;
+#pragma comment (lib, "Ws2_32.lib")
 
+#define DEFAULT_PORT "80"
 
 namespace server {
 
-    unsigned char bridge_ip1, bridge_ip2, bridge_ip3, bridge_ip4;
+    struct addrinfo *result = NULL;
+    struct addrinfo hints;
 
-    std::string key;
+    SOCKET getListenSocket(void) {
 
-    unsigned __stdcall Answer(void* a) {
-        Socket* s = (Socket*) a;
-        while (1) {
-            std::string r = s->ReceiveLine();
-            //std::cout << "Reciveline:" << r << std::endl;
-            if (r.empty()) break;
-            s->SendLine(r);
-        }
-        delete s;
+        WSADATA wsaData;
+        // Iniciar
+        WSAStartup(MAKEWORD(2,2), &wsaData);
+
+        ZeroMemory(&hints, sizeof(hints));
+        hints.ai_family = AF_INET;
+        hints.ai_socktype = SOCK_STREAM;
+        hints.ai_protocol = IPPROTO_TCP;
+        hints.ai_flags = AI_PASSIVE;
+
+        // Resolve the server address and port
+        getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
+       
+        return socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+    }
+
+    int _listen(SOCKET listenSocket) {
+        std::cout << "[LOG] listen started" << std::endl;
+        // Setup the TCP listening socket
+        bind(listenSocket, result->ai_addr, (int)result->ai_addrlen);
+
+        freeaddrinfo(result);
+
+        listen(listenSocket, SOMAXCONN);
+
+        SOCKET clientSocket;
+        // Accept a client socket
+        clientSocket = accept(listenSocket, NULL, NULL);
+
+
+        // Receive until the peer shuts down the connection
+        int i, r;
+
+        //CHAR------------------------
+        int recvbuf_lenght = 20;
+        char recvbuf[recvbuf_lenght];
+
+        char respuesta[20] = "respuesta";
+        do {
+            i = recv(clientSocket, recvbuf, recvbuf_lenght, 0);
+            
+            if (i > 0) {
+                printf("Bytes received: %d\n", i);
+                std::cout << "Message: " << recvbuf << std::endl;
+                std::cout << "escribir espuesta: ";
+
+                char x[3];
+                std::cin >> x;
+                
+                send(clientSocket, x, 3, 0);
+
+                printf("Bytes sent: %d\n", r);
+            }
+            
+        } while (i > 0);
+
         return 0;
-    }
-
-    int listen() {
-        SocketServer in(2000,5);
-
-        while (1) {
-            Socket* s=in.Accept();
-
-            unsigned ret;
-            _beginthreadex(0,0,Answer,(void*) s,0,&ret);
-        }
-
-        return 0;
-    }
-
-    bool connected;
-
-    bool bridgeConnection;
-
-    void init() {
-        connected = false;
-    }
-
-    void init(std::string _key) {
-        key = _key;
-        connected = false;
-    }
-
-    bool authenticate();
-
-    bool setBridge();
-
-    void saveConfig();
-
-    void loadConfig();
-
-    std::string setKey(std::string x) {
-        key = x;
-        return key;
-    }
-
-    bool isConnected() {
-        return connected;
     }
 
 }
--- main.cpp
+++ main.cpp
@@ -1,28 +1,28 @@
-#include "Log.h"
+//#include "Log.h"
 #include "Server.h"
 
 #include <iostream>
-#include <string>
+
 
 int main(int argc, char* argv[]) {
 
-    log::createLog();
+    //log::createLog();
 
     switch (argc) {
     case 1:
-        log::log("KEY no introducida", false);
-        server::init();
+        //log::log("KEY no introducida", false);
+        //server::init();
         break;
     case 2:
-        log::log("KEY: " + std::string(argv[1]), false);
-        server::init(argv[1]);
+        //log::log("KEY: " + std::string(argv[1]), false);
+        //server::init(argv[1]);
         break;
     default:
-        log::log("Parametros incorrectos", true);
+        //log::log("Parametros incorrectos", true);
         return 0;
     }
 
-    server::listen();
-    
-    return 0;
+    server::_listen(server::getListenSocket());
+
+    return 0; 
 }
